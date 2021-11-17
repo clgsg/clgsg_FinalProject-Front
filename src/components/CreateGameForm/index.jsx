8baf9a61ba9b58ee6f994a548d036f536d9b54e0
clgsg <clg_listas@yahoo.es>
2021-11-17 19:23:11 +0100

    rebase

---
+++ src/components/CreateGameForm/index.jsx
@@ -0,0 +1,66 @@
+import { Select } from "@chakra-ui/react";
+import Button from "components/Button";
+import Calendar from "components/DateAndTime/Calendar.jsx";
+
+function FilterGameForm() {
+	return (
+		<>
+			<h2>Qué</h2>
+			<Select
+				variant="outline"
+				size="sm"
+				borderColor="teal"
+				placeholder="Selecciona el deporte"
+			>
+				<option value="option1">3x3</option>
+				<option value="option2">Baloncesto</option>
+				<option value="option3">Fútbol</option>
+				<option value="option4">Fútbol sala</option>
+				<option value="option5">Fútbol 7</option>
+				<option value="option6">Pádel</option>
+				<option value="option7">Tenis</option>
+				<option value="option8">Otro</option>
+			</Select>
+			<h2>Fecha</h2>
+			<Calendar></Calendar>
+			<h2>A partir de las</h2>
+			<h2>Deporte adaptado</h2>
+
+			<h2>Hombres/Mujeres/Mixto</h2>
+
+			<Select
+				variant="outline"
+				size="sm"
+				borderColor="teal"
+				placeholder="Selecciona el lugar"
+			>
+				<option value="option1">IDB 2.100 PAU</option>
+				<option value="option2">IDB 2.75 PAU</option>
+				<option value="option3">IDB 3.16 PAU</option>
+				<option value="option4">IDB Ademo (Arroyo Fontarrón)</option>
+				<option value="option5">IDB Alameda de Osuna I</option>
+				<option value="option6">IDB Alameda de Osuna II</option>
+				<option value="option7">IDB Alas</option>
+				<option value="option8">IDB Albaicín</option>
+				<option value="option9">IDB Albaida</option>
+				<option value="option10">IDB Alcalde Martín de Alzaga</option>
+				<option value="option11">IDB Alerce</option>
+				<option value="option12">IDB Alfonso XIII</option>
+				<option value="option13">IDB Alhaurín</option>
+				<option value="option14">IDB Alicún</option>
+				<option value="option15">IDB Aliseda</option>
+				<option value="option16">IDB Almudena</option>
+				<option value="option17">IDB Ambulatorio (El Paraíso)</option>
+				<option value="option18">IDB Americio</option>
+			</Select>
+			<Button className="ClearBttn" text="Borrar" to=""></Button>
+			<Button
+				className="FilterBttn"
+				text="Buscar"
+				to="browseGamesResults"
+			></Button>
+		</>
+	);
+}
+
+export default FilterGameForm;
