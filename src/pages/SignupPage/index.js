d78eced5297d64795080a63794dbafb313f2cd32
clgsg <clg_listas@yahoo.es>
2021-11-24 12:36:04 +0100

    fix: links to login or signup pages

--- src/pages/SignupPage/index.js
+++ src/pages/SignupPage/index.js
@@ -6,7 +6,7 @@ const SignupPage = () => {
 		<>
 			<SignupForm />
 			<p>
-				¿Ya tienes cuenta? <Link href="/login">Ve a la página de acceso</Link>
+				¿Ya tienes cuenta? <Link to="login">Ve a la página de acceso</Link>
 			</p>
 		</>
 	);
