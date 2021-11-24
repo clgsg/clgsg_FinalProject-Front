96e526e974e6acb6748072199c7107b11bd95b2d
clgsg <clg_listas@yahoo.es>
2021-11-24 12:38:52 +0100

    chore: update header styling

--- src/components/TopHeader/index.jsx
+++ src/components/TopHeader/index.jsx
@@ -6,15 +6,13 @@ export default function TopHeader() {
 		<>
 		<div className="header">
 			<div className="headerTitle">
-				<h1>megustalapachanga</h1>
-			{/* </div>
-			<div> */}
-				<img
+				<h1><img
 					src="https://www.imdsg.es/wp-content/uploads/fondo.jpg"
 					alt="Balones y pelotas"
 					width="200vh"
 					height="80vh"
 				/>
+				megustalapachanga</h1>
 			</div>
 		</div>
 		</>
