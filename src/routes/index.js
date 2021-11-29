09011f01e7f389f4cb606ff70fff68cc1dc44771
clgsg <clg_listas@yahoo.es>
2021-11-29 10:20:46 +0100

    fixing vercel deployment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 // import UserGames from "components/UserGames";
 // import BrowseGames from "pages/browseGames";
 // import BrowseGamesResults from "pages/browseGamesResults";
-import CreateGame from "pages/createGame";
+// import CreateGame from "pages/createGame";
 import ForgotPassword from "pages/forgotPassword";
 import GameDetails from "pages/gameDetails";
 import Homepage from "pages/homepage";
 import LoginPage from "pages/loginPage";
 import SignupPage from "pages/signupPage";
-import UpdateEmail from "pages/updateEmail";
-import UpdatePassword from "pages/updateEmail";
-import UpdatePic from "pages/updatePic";
+// import UpdateEmail from "pages/updateEmail";
+// import UpdatePassword from "pages/updateEmail";
+// import UpdatePic from "pages/updatePic";
 import UserTabs from "pages/userTabs";
 
 const Routes = [
@@ -23,11 +23,11 @@ const Routes = [
 	// 	component: BrowseGamesResults,
 	// 	exact: true,
 	// },
-	{
-		path: "/games/new",
-		component: CreateGame,
-		exact: true,
-	},
+	// {
+	// 	path: "/games/new",
+	// 	component: CreateGame,
+	// 	exact: true,
+	// },
 	{
 		path: "/auth/pwd/forgotten",
 		component: ForgotPassword,
@@ -53,21 +53,21 @@ const Routes = [
 		component: SignupPage,
 		exact: true,
 	},
-	{
-		path: "/auth/email/update",
-		component: UpdateEmail,
-		exact: true,
-	},
-	{
-		path: "/auth/pwd/update",
-		component: UpdatePassword,
-		exact: true,
-	},
-	{
-		path: "/users/030fbdfa-ad94-42bd-9633-ee4fa1bf3631/updatepic",
-		component: UpdatePic,
-		exact: true,
-	},
+	// {
+	// 	path: "/auth/email/update",
+	// 	component: UpdateEmail,
+	// 	exact: true,
+	// },
+	// {
+	// 	path: "/auth/pwd/update",
+	// 	component: UpdatePassword,
+	// 	exact: true,
+	// },
+	// {
+	// 	path: "/users/030fbdfa-ad94-42bd-9633-ee4fa1bf3631/updatepic",
+	// 	component: UpdatePic,
+	// 	exact: true,
+	// },
 	{
 		path: `/users/030fbdfa-ad94-42bd-9633-ee4fa1bf3631/:tab?`,
 		render: "{props => <App {...props}/>}",
